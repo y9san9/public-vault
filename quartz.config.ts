e8c143afb3a6a491db7a98fcd26f459752ef259b
y9san9 / Alex Sokol <y9san9@gmail.com>
2025-11-20 20:02:07 +0300

    Quartz sync: Nov 20, 2025, 8:02 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -87,7 +87,7 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
-      Plugin.CustomOgImages(),
+      // Plugin.CustomOgImages(),
     ],
   },
 }
--- quartz/plugins/emitters/ogImage.tsx
+++ quartz/plugins/emitters/ogImage.tsx
@@ -153,8 +153,11 @@ export const CustomOgImages: QuartzEmitterPlugin<Partial<SocialImageOptions>> = 
                 : `https://${baseUrl}/static/${userDefinedOgImagePath}`
             }
 
-            const ogImagePath = userDefinedOgImagePath
-            if (!ogImagePath) return
+            const generatedOgImagePath = isRealFile
+              ? `https://${baseUrl}/${pageData.slug!}-og-image.webp`
+              : undefined
+            const defaultOgImagePath = `https://${baseUrl}/static/og-image.png`
+            const ogImagePath = userDefinedOgImagePath ?? generatedOgImagePath ?? defaultOgImagePath
             const ogImageMimeType = `image/${getFileExtension(ogImagePath) ?? "png"}`
             return (
               <>
