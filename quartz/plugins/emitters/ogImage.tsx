a616fb4e99a5621d52737cd52ea573206af33f18
y9san9 / Alex Sokol <y9san9@gmail.com>
2025-11-20 19:45:55 +0300

    Quartz sync: Nov 20, 2025, 7:45 PM

--- quartz/plugins/emitters/ogImage.tsx
+++ quartz/plugins/emitters/ogImage.tsx
@@ -156,8 +156,7 @@ export const CustomOgImages: QuartzEmitterPlugin<Partial<SocialImageOptions>> = 
             const generatedOgImagePath = isRealFile
               ? `https://${baseUrl}/${pageData.slug!}-og-image.webp`
               : undefined
-            const defaultOgImagePath = `https://${baseUrl}/static/og-image.png`
-            const ogImagePath = userDefinedOgImagePath ?? generatedOgImagePath ?? defaultOgImagePath
+            const ogImagePath = userDefinedOgImagePath ?? generatedOgImagePath
             const ogImageMimeType = `image/${getFileExtension(ogImagePath) ?? "png"}`
             return (
               <>
