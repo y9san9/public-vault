25823b81272e70d58df2aa7c7bd8e8980449538a
y9san9 / Alex Sokol <y9san9@gmail.com>
2025-12-14 15:43:54 +0300

    Quartz sync: Dec 14, 2025, 3:43 PM

--- content/metric-time.js
+++ content/metric-time.js
@@ -3,14 +3,13 @@ const stMillisecondsInMTSecond = stMillisecondsInStDay / 10 / 100 / 100;
 
 const metricTime = document.getElementById('metric-time');
 
-safeSetInterval(() => {
+setInterval(() => {
     // Date.now() returns since Unix, I need in local timezone
     const now = new Date();
     const stMilliseconds = now.getHours() * 3600_000
                          + now.getMinutes() * 60_000
                          + now.getSeconds() * 1000
                          + now.getMilliseconds();
-
     const mtSeconds = stMilliseconds / stMillisecondsInMTSecond;
     const mtSecondsString = String(Math.trunc(mtSeconds % 100))
         .padStart(2, '0');
@@ -21,15 +20,3 @@ safeSetInterval(() => {
     const mtHoursString = String(Math.trunc(mtHours)).padStart(2, '0');
     metricTime.textContent = `${mtHoursString}:${mtMinutesString}:${mtSecondsString} MT`;
 }, stMillisecondsInMTSecond / 2);
-
-function safeSetInterval(callback, timeout, drift) {
-    let correctedTimeout = timeout + (drift ?? 0);
-    correctedTimeout = Math.max(correctedTimeout, 0);
-    const expectedDate = Date.now() + correctedTimeout;
-    setTimeout(() => {
-        callback();
-        const drift = Date.now() - expectedDate;
-        safeSetInterval(callback, timeout, drift);
-    }, correctedTimeout);
-}
-
